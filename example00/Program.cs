b146cb3a0325dd595968d1c1880642df679ff0f7
Alex <alexfil0606@gmail.com>
2022-12-25 11:34:23 +0300

    1

---
+++ example00/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+string[] CreatrArrayString()
+{
+    Console.Write("Введите число для определения размера массива  ");
+    int arg = Convert.ToInt32(Console.ReadLine());
+
+    string[] arr = new string[arg];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("Введите строку  ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+string[] array = CreatrArrayString();
+
+
+string[] Sorting(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length >= 3) count++;
+    }
+
+    string[] newArr = new string[count];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length >= 3)
+        {
+            newArr[j] = arr[i];
+            j++;
+        }
+    }
+    return newArr;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+PrintArray(array);
+Console.Write(" -> ");
+
+
+string[] newArray = Sorting(array);
+PrintArray(newArray);
